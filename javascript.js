34cd16f4169717b0443a770f0760d9b2da0ce78f
Ben Morrison <ben@Bens-MacBook-Air-3.local>
2024-05-29 11:22:34 -0400

    updated css website with contact and success

--- javascript.js
+++ javascript.js
@@ -18,3 +18,13 @@ function myFunction() {
     header.classList.remove("sticky");
   }
 }
+
+const form = document.querySelector('#form');
+const submitButton = document.querySelector('#submit');
+
+form.addEventListener('submit', (e) => {
+  submitButton.disabled = true;
+  e.preventDefault();
+  window.location.href = window.location.origin + '/success.html';
+});
+
